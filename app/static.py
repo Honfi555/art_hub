e86d1032ce1b7fee97d7c055d3536ccf416c6971
kasan <kasanindaniil@gmail.com>
2025-04-19 16:33:34 +1000

    update static configuration to include Redis settings and image base URL

--- app/static.py
+++ app/static.py
@@ -5,4 +5,10 @@ dotenv.load_dotenv(".env")
 
 SECRET_KEY: str = os.getenv("SECRET_KEY")
 ALGORITHM: str = os.getenv("ALGORITHM")
-DATA_SOURCE: str = os.getenv("DATA_SOURCE")
+POSTGRES_SOURCE: str = os.getenv("POSTGRES_SOURCE")
+REDIS_HOST: str = os.getenv("REDIS_HOST")
+REDIS_PORT: int = int(os.getenv("REDIS_PORT")) if isinstance(os.getenv("REDIS_PORT"), str) else 6379
+REDIS_DB: int = int(os.getenv("REDIS_DB")) if isinstance(os.getenv("REDIS_DB"), str) else 0
+REDIS_PASSWORD: str = os.getenv("REDIS_PASSWORD")
+IMAGE_BASE_URL: str = os.getenv("IMAGE_BASE_URL", '')
+REDIS_USER: str = os.getenv("REDIS_USER")
