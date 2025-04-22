4406bebd3ff0e33de6af0c97c390877bcf99e1c9
kasan <kasanindaniil@gmail.com>
2025-04-22 15:14:55 +1000

    rename tittle column to title

--- app/database/articles.py
+++ app/database/articles.py
@@ -21,14 +21,13 @@ def select_articles_announcement(amount: Optional[int] = None,
 		conn = connect_pg()
 		with conn.cursor() as cur:
 			query = """
-				SELECT 
-					art.article_id,
-					art.tittle,
-					us.login,
-					art.announcement
-				FROM articles.articles art
-				JOIN users.users us ON art.user_id = us.id
-			"""
+                    SELECT art.article_id,
+                           art.title,
+                           us.login,
+                           art.announcement
+                    FROM articles.articles art
+                             JOIN users.users us ON art.user_id = us.id
+					"""
 			params = []
 			if login:
 				query += "WHERE us.login = %s"
@@ -62,15 +61,14 @@ def select_article(article_id: int) -> ArticleData | None:
 		conn = connect_pg()
 		with conn.cursor() as cur:
 			query = """
-				SELECT 
-					art.article_id,
-					art.tittle,
-					us.login,
-					art.article_body
-				FROM articles.articles art
-				JOIN users.users us ON art.user_id = us.id
-				WHERE article_id = %s
-			"""
+                    SELECT art.article_id,
+                           art.title,
+                           us.login,
+                           art.article_body
+                    FROM articles.articles art
+                             JOIN users.users us ON art.user_id = us.id
+                    WHERE article_id = %s;
+					"""
 			cur.execute(query, (article_id,))
 			result = cur.fetchone()
 			logger.info("Получена статься с id %s", article_id)
@@ -93,16 +91,15 @@ def select_article_full(article_id: int) -> ArticleFull | None:
 		conn = connect_pg()
 		with conn.cursor() as cur:
 			query = """
-				SELECT 
-					art.article_id,
-					art.tittle,
-					us.login,
-					art.announcement,
-					art.article_body
-				FROM articles.articles art
-				JOIN users.users us ON art.user_id = us.id
-				WHERE article_id = %s
-			"""
+                    SELECT art.article_id,
+                           art.title,
+                           us.login,
+                           art.announcement,
+                           art.article_body
+                    FROM articles.articles art
+                             JOIN users.users us ON art.user_id = us.id
+                    WHERE article_id = %s;
+					"""
 			cur.execute(query, (article_id,))
 			result = cur.fetchone()
 			logger.info("Получена полная статься, с id %s", article_id)
@@ -125,12 +122,11 @@ def insert_article(article: ArticleFull) -> int | None:
 		conn = connect_pg()
 		with conn.cursor() as cur:
 			query = """
-					INSERT INTO articles.articles
-					(tittle, user_id, announcement, article_body)
-					VALUES
-					(%s, (SELECT id FROM users.users WHERE login = %s), %s, %s)
-					RETURNING article_id;
-			"""
+                    INSERT INTO articles.articles
+                        (title, user_id, announcement, article_body)
+                    VALUES (%s, (SELECT id FROM users.users WHERE login = %s), %s, %s)
+                    RETURNING article_id;
+					"""
 			data = (article.title, article.user_name, article.announcement, article.article_body)
 			cur.execute(query, data)
 			result = cur.fetchone()[0]
@@ -155,10 +151,12 @@ def update_article(article: ArticleFull) -> None:
 		conn = connect_pg()
 		with conn.cursor() as cur:
 			query = """
-				UPDATE articles.articles 
-				SET tittle = %s, article_body = %s, announcement = %s
-				WHERE article_id = %s
-			"""
+                    UPDATE articles.articles
+                    SET title        = %s,
+                        article_body = %s,
+                        announcement = %s
+                    WHERE article_id = %s;
+					"""
 			data = (article.title, article.article_body, article.announcement, article.id)
 			cur.execute(query, data)
 			conn.commit()
@@ -181,9 +179,10 @@ def delete_article(article_id: int) -> None:
 		conn = connect_pg()
 		with conn.cursor() as cur:
 			query = """
-				DELETE FROM articles.articles
-				WHERE article_id = %s
-			"""
+                    DELETE
+                    FROM articles.articles
+                    WHERE article_id = %s;
+					"""
 			cur.execute(query, (article_id,))
 			conn.commit()
 			logger.info("Удалена статья %s", article_id)
