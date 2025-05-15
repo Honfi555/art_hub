6c8f6c9e176005c0eab43c481c64b60accad81f4
Honfi555 <kasanindaniil@gmail.com>
2025-05-15 20:26:27 +1000

    convert spaces to tabs

--- app/database/articles.py
+++ app/database/articles.py
@@ -10,6 +10,7 @@ from ..models.articles import ArticleData, ArticleAnnouncement, ArticleFull
 
 __all__: list[str] = ["select_articles_announcement", "select_article", "select_article_full", "insert_article",
 					  "update_article", "delete_article", "select_articles_by_search"]
+
 logger: Logger = configure_logs(__name__)
 
 
@@ -22,12 +23,12 @@ def select_articles_announcement(amount: Optional[int] = None,
 		conn = connect_pg()
 		with conn.cursor() as cur:
 			query = """
-					SELECT art.article_id,
-						   art.title,
-						   us.login,
-						   art.announcement
-					FROM articles.articles art
-							 JOIN users.users us ON art.user_id = us.id
+                    SELECT art.article_id,
+                           art.title,
+                           us.login,
+                           art.announcement
+                    FROM articles.articles art
+                             JOIN users.users us ON art.user_id = us.id
 					"""
 			params = []
 			if login:
@@ -61,13 +62,13 @@ def select_article(article_id: int) -> ArticleData | None:
 		conn = connect_pg()
 		with conn.cursor() as cur:
 			query = """
-					SELECT art.article_id,
-						   art.title,
-						   us.login,
-						   art.article_body
-					FROM articles.articles art
-							 JOIN users.users us ON art.user_id = us.id
-					WHERE article_id = %s;
+                    SELECT art.article_id,
+                           art.title,
+                           us.login,
+                           art.article_body
+                    FROM articles.articles art
+                             JOIN users.users us ON art.user_id = us.id
+                    WHERE article_id = %s;
 					"""
 			cur.execute(query, (article_id,))
 			result = cur.fetchone()
@@ -91,14 +92,14 @@ def select_article_full(article_id: int) -> ArticleFull | None:
 		conn = connect_pg()
 		with conn.cursor() as cur:
 			query = """
-					SELECT art.article_id,
-						   art.title,
-						   us.login,
-						   art.announcement,
-						   art.article_body
-					FROM articles.articles art
-							 JOIN users.users us ON art.user_id = us.id
-					WHERE article_id = %s;
+                    SELECT art.article_id,
+                           art.title,
+                           us.login,
+                           art.announcement,
+                           art.article_body
+                    FROM articles.articles art
+                             JOIN users.users us ON art.user_id = us.id
+                    WHERE article_id = %s;
 					"""
 			cur.execute(query, (article_id,))
 			result = cur.fetchone()
@@ -122,10 +123,9 @@ def insert_article(article: ArticleFull) -> int | None:
 		conn = connect_pg()
 		with conn.cursor() as cur:
 			query = """
-					INSERT INTO articles.articles
-						(title, user_id, announcement, article_body)
-					VALUES (%s, (SELECT id FROM users.users WHERE login = %s), %s, %s)
-					RETURNING article_id;
+                    INSERT INTO articles.articles
+                        (title, user_id, announcement, article_body)
+                    VALUES (%s, (SELECT id FROM users.users WHERE login = %s), %s, %s) RETURNING article_id;
 					"""
 			data = (article.title, article.user_name, article.announcement, article.article_body)
 			cur.execute(query, data)
@@ -151,13 +151,14 @@ def update_article(article: ArticleFull) -> None:
 		conn = connect_pg()
 		with conn.cursor() as cur:
 			query = """
-					UPDATE articles.articles
-					SET title        = %s,
-						article_body = %s,
-						announcement = %s
-					WHERE article_id = %s;
+                    UPDATE articles.articles
+                    SET title        = %s,
+                        article_body = %s,
+                        announcement = %s
+                    WHERE article_id = %s;
 					"""
-			data = (article.title, article.article_body, article.announcement, article.id)
+			data = (article.title, article.article_body,
+					article.announcement, article.id)
 			cur.execute(query, data)
 			conn.commit()
 			logger.info("Обновлена статья, с id %s", article.id)
@@ -179,9 +180,9 @@ def delete_article(article_id: int) -> None:
 		conn = connect_pg()
 		with conn.cursor() as cur:
 			query = """
-					DELETE
-					FROM articles.articles
-					WHERE article_id = %s;
+                    DELETE
+                    FROM articles.articles
+                    WHERE article_id = %s;
 					"""
 			cur.execute(query, (article_id,))
 			conn.commit()
@@ -198,11 +199,11 @@ def delete_article(article_id: int) -> None:
 
 
 def select_articles_by_search(
-	query_str: str,
-	amount: Optional[int] = None,
-	chunk: Optional[int] = None,
-	login: Optional[str] = None,
-	announcement: Optional[bool] = None
+		query_str: str,
+		amount: Optional[int] = None,
+		chunk: Optional[int] = None,
+		login: Optional[str] = None,
+		announcement: Optional[bool] = None
 ) -> list[dict]:
 	"""
 	Выполняет «умный» поиск по title, announcement и article_body,
@@ -214,30 +215,27 @@ def select_articles_by_search(
 		with conn.cursor(cursor_factory=RealDictCursor) as cur:
 			# подготовка CTE для полнотекстового и trigram поиска
 			sql = """
-			WITH q AS (
-				SELECT plainto_tsquery('russian', %s) AS tsq,
-					   %s::text                       AS rawq
-			),
-			fts AS (
-				SELECT article_id,
-					   ts_rank_cd(search_vector, q.tsq) AS rank_fts
-				FROM articles.articles, q
-				WHERE search_vector @@ q.tsq
-			),
-			trgm AS (
-				SELECT article_id,
-					   greatest(
-						   similarity(title, q.rawq),
-						   similarity(announcement, q.rawq),
-						   similarity(article_body, q.rawq)
-					   ) AS rank_trgm
-				FROM articles.articles, q
-				WHERE (coalesce(title, '') || ' ' ||
-					   coalesce(announcement, '') || ' ' ||
-					   coalesce(article_body, ''))
-					  %% q.rawq
-			)
-			"""
+                  WITH q AS (SELECT plainto_tsquery('russian', %s) AS tsq,
+                                    %s::text                       AS rawq),
+                       fts AS (SELECT article_id,
+                                      ts_rank_cd(search_vector, q.tsq) AS rank_fts
+                               FROM articles.articles,
+                                    q
+                               WHERE search_vector @@ q.tsq
+                      ), trgm AS (
+                  SELECT article_id, greatest(
+                      similarity(title, q.rawq), similarity(announcement, q.rawq), similarity(article_body, q.rawq)
+                      ) AS rank_trgm
+                  FROM articles.articles, q
+                  WHERE (coalesce (title
+                      , '') || ' ' ||
+                      coalesce (announcement
+                      , '') || ' ' ||
+                      coalesce (article_body
+                      , ''))
+                      %% q.rawq
+                      ) \
+				  """
 			# динамическое формирование списка столбцов
 			select_cols = [
 				'a.article_id',
